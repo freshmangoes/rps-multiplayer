d9784fab208c38d4e7ce2bb9b18826a27af60b5e
Kyle C <kylecilia@gmail.com>
2019-11-10 19:13:37 -0800

    Moving to another machine [WIP]

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,34 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 var db = firebase.database();
+var connectionsRef = db.ref("/connections");
+var connectedRef = db.ref(".info/connected");
+
+connectedRef.on("value", (snap) => {
+  if(snap.val()) {
+    var con = connectionsRef.push(true);
+    con.onDisconnect().remove();
+  }
+});
+
+connectionsRef.on("value", (snap) => {
+  console.log('connectionsRef snap', snap);
+  console.log("Number of connections:", snap.numChildren());
+  $(".connected-players").text(snap.numChildren());
+});
 
 var player1 = {
-
+  connected: false,
+  move: null,
 }
 
 var player2 = {
-  
+  connected: false,
+  move: null,
 }
+
+db.ref().on("value", (snap) => {
+  console.log("Snapshot:", snap);
+}, (errorObj) => {
+  console.log("Read failed:", errorObj.code);
+});
