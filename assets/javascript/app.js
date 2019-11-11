da45afefada84748ee6aa0314248a6289ab89a7c
Kyle C <kylecilia@gmail.com>
2019-11-10 18:12:26 -0800

    Initial files and firebase config

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyARmLVFjqNcIup_N3sZgcF5vYXmbOXf-Ds",
+  authDomain: "rps-multiplayer-aee22.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-aee22.firebaseio.com",
+  projectId: "rps-multiplayer-aee22",
+  storageBucket: "rps-multiplayer-aee22.appspot.com",
+  messagingSenderId: "797060947172",
+  appId: "1:797060947172:web:b563eec21648a66e9b60f1",
+  measurementId: "G-9R51M0G1YC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.database();
+
+var player1 = {
+
+}
+
+var player2 = {
+  
+}
